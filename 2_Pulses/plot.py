6aa0819a8fb02f17043c677f2066e10d6ec8b158
Jason Yip <jason17@berkeley.edu>
2025-09-16 11:02:49 -0700

    add files to 2,3 via upload

--- 2_Pulses/plot.py
+++ 2_Pulses/plot.py
@@ -1,26 +1,53 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
-
 import os, sys
+import pickle
+import pandas as pd
 
 p = os.path.abspath('../')
 if p not in sys.path:
     sys.path.append(p)
+from pulse_batch import PulseGenerator, PulseBatch
 from rcparams import plotter, textwidth, columnwidth
 
 @plotter()
 def main():
     
-    fig, axes = plt.subplots(figsize=(textwidth,textwidth*0.6), nrows=2, ncols=2, dpi=130)
+    n_channels = 1024
+    sampling_rate = 400e6  # Hz
+    dt = 1 / sampling_rate  # seconds per channel/sample
+    t_n = np.arange(n_channels) * dt * 1e6
+
+    gen = PulseGenerator()
+    slow_rises, _ = gen.simulate_pulses(N_PULSES=10, N_SAMPLES=1024,
+                                        PROB_NORMAL=0, PROB_DOUBLE_STEP=0,PROB_FLAT_TOP=0,PROB_SLOW_RISER=1)
+    nominal_pulses, _ = gen.simulate_pulses(N_PULSES=10, N_SAMPLES=1024,
+                                            PROB_NORMAL=1, PROB_DOUBLE_STEP=0,PROB_FLAT_TOP=0,PROB_SLOW_RISER=0)
+    pile_ups, _ = gen.simulate_pulses(N_PULSES=10, N_SAMPLES=1024,
+                                      PROB_NORMAL=0, PROB_DOUBLE_STEP=1,PROB_FLAT_TOP=0,PROB_SLOW_RISER=0)
+    flat_tops, _ = gen.simulate_pulses(N_PULSES=10, N_SAMPLES=1024,
+                                       PROB_NORMAL=0, PROB_DOUBLE_STEP=0,PROB_FLAT_TOP=1,PROB_SLOW_RISER=0)
+
+    slow_rise = slow_rises[np.random.choice(len(slow_rises))]
+    nominal_pulse = nominal_pulses[np.random.choice(len(nominal_pulses))]
+    pile_up = pile_ups[np.random.choice(len(pile_ups))]
+    flat_top = flat_tops[np.random.choice(len(flat_tops))]
+    
+    fig, axes = plt.subplots(figsize=(textwidth,textwidth*0.6), nrows=2, ncols=2, dpi=130, sharex=True)
 
     ax = axes[0,0]
-    ax.plot()
+    ax.plot(t_n, nominal_pulse, color='black')
+    ax.set_ylabel('Amplitude (A.U.)')
+    ax.set_xlabel('Time (µs)')
 
-    ax.set_xlim()
-    ax.set_xlabel('')
-    ax.set_ylim()
-    ax.set_ylabel('')
+    ax = axes[0,1]
+    ax.plot(t_n, slow_rise, color='black')
+
+    ax = axes[1,0]
+    ax.plot(t_n, pile_up, color='black')
+
+    ax = axes[1,1]
+    ax.plot(t_n, flat_top, color='black')
 
     plt.tight_layout()
     plt.savefig(f'{os.path.basename(os.getcwd())}.png',  bbox_inches = 'tight', pad_inches = 0.1, dpi=300)
--- 3_DAE/plot.py
+++ 3_DAE/plot.py
@@ -3,23 +3,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 import os, sys
+import pickle
+import pandas as pd
 
 p = os.path.abspath('../')
 if p not in sys.path:
     sys.path.append(p)
+from pulse import Pulse
 from rcparams import plotter, textwidth, columnwidth
 
 @plotter()
 def main():
+
+    n_channels = 1024
+    sampling_rate = 400e6  # Hz
+    dt = 1 / sampling_rate  # seconds per channel/sample
+    t_n = np.arange(n_channels) * dt * 1e6
+
+    with open('../denoise_data21_v2.pkl', 'rb') as f:
+        df21 = pickle.load(f)
     
-    fig, ax = plt.subplots(figsize=(textwidth,textwidth*0.6), nrows=1, ncols=1, dpi=130)
+    i=6747
+    ori_pulse = df21['original_normalized_pulse'].iloc[i]
+    recon = df21['recon_pulse'].iloc[i]
+    recon_deriv = df21['original_deriv_norm'].iloc[i]
+    
+    fig, axes = plt.subplots(figsize=(textwidth,textwidth*0.4), nrows=1, ncols=2, dpi=130)
+    axes = axes.flatten()
 
-    ax.plot()
+    ax = axes[0]
+    ax.plot(t_n[:-100], ori_pulse, label='Original', alpha=0.5)
+    ax.plot(t_n[:-100], recon, label='Reconstructed', color='black')
+    ax.legend()
+    
+    ax.set_ylabel('Amplitude (A.U.)')
+    ax.set_xlabel('Time (µs)')
 
-    ax.set_xlim()
-    ax.set_xlabel('')
-    ax.set_ylim()
-    ax.set_ylabel('')
+    ax = axes[1]
+    ax.plot(t_n[:-100], Pulse(ori_pulse).normalize_deriv(), label='Original', alpha=0.5)
+    ax.plot(t_n[:-100], recon_deriv, label='Reconstructed', color='black')
+    ax.legend()
+    
+    ax.set_xlabel('Time (µs)')
+    
 
     plt.tight_layout()
     plt.savefig(f'{os.path.basename(os.getcwd())}.png',  bbox_inches = 'tight', pad_inches = 0.1, dpi=300)
